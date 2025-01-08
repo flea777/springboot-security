857b148fc1396837ebb333f256036303274f507d
Pedro Bim <pedrodinizz2@outlook.com>
2025-01-08 02:12:59 -0300

    Initial config

---
+++ src/test/java/io/github/flea777/sboot_security/SbootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.flea777.sboot_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbootSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
