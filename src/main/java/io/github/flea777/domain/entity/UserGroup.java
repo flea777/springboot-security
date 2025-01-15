c32ad730b62ce6e4494484daf813d2bb65f1e010
Pedro Bim <pedrodinizz2@outlook.com>
2025-01-14 22:50:00 -0300

    Entities created

---
+++ src/main/java/io/github/flea777/domain/entity/UserGroup.java
@@ -0,0 +1,26 @@
+package io.github.flea777.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Entity
+@Data
+public class UserGroup {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "group_id")
+    private Group group;
+}
